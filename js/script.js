6514d6fe34dd611ab13ffa0c1bfac460e79f4192
warrensamu <116026091+warrensamu@users.noreply.github.com>
2024-02-16 14:43:59 -0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,93 @@
+// STEP 1
+// let someMonth
+// function theMonth()
+// let currentMonth
+// let summerMonth
+// let myLibraryFunction
+// STEP 2
+// let myNumber = 4
+// let myString = 'Hello'
+// let myBoolean = true
+// let myNull = null
+// STEP 3
+// let myComplex = 3 + 5
+// let myVariable = 7 - 8
+// STEP 4
+// let firstName
+// let lastName
+// let address
+// let city
+// let state
+// let zipCode
+// let yourAge
+// let referralSource
+// let mayWeContactYou
+//STEP 5
+// myName = 'Warren'
+// mayWeContactYou = true
+// zipCode = 92120
+// let myName = 'Warren'
+// let mayWeContactYou = true
+// let zipCode = 92120
+// let myName = 'Warren', mayWeContactYou = true, zipCode = 92120
+// STEP 6
+// let myAge = 'Warren is ' + 40
+// console.log(myAge)
+//STEP 7
+// let likesGaming = 'Warren likes gaming? ' + true
+// console.log(likesGaming)
+// let milesWalked = 1000 + true
+// console.log(milesWalked)
+//STEP 8
+// let someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and ' + "I'm" + 'not sure about the former."'
+// console.log(someString)
+//STEP 9
+// let myVar = null
+// console.log(myVar)
+// let myValue
+// console.log(myValue)
+//STEP 10
+// console.log(typeof 'Hello')
+// console.log(typeof 4)
+// console.log(typeof false)
+// console.log(typeof Object())
+// console.log(typeof myVar)
+//STEP 11
+//alert('Hello ' + 'Warren Samu, ' + 'welcome to the JavaScript class!')
+//STEP 12
+//let myName = 'Warren Samu'
+//alert('Hello ' + myName + ', welcome to the JavaScript class!')
+//STEP 13
+//let myCourse = "JavaScript"
+//alert('Hello ' + myName + ', welcome to the ' + myCourse + ' class!')
+//STEP 14
+//alert('Hello ' + myName + '.\nWelcome to the ' + myCourse + ' class!')
+//STEP 15
+// let myName = prompt('My name is')
+//STEP 16
+// let myClass = prompt('My current class is')
+// alert('Hello ' + myName + '.\nWelcome to the ' + myClass + ' class!')
+//STEP 17
+// let x = 10
+// let y = 20
+// console.log(x + y)
+//STEP 18
+// let x = 20
+// x += 20
+// console.log(x)
+//STEP 19
+// let x = 20
+// x *= 5
+// console.log(x)
+//STEP 20
+// let x = 20 % 3
+// x /= 1
+// console.log(x)
+//STEP 21
+// let x = 10
+// let y = 20
+// console.log(y >= 15 && x <= 11)
+//STEP 22
+// let x = 10
+// let y = 20
+// console.log(x === 11 || y !== 20 )
